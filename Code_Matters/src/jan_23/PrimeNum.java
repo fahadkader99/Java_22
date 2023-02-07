12f39ec92b531fc4fef1ec0bbc2a4b18e1cd6a05
Fahad_Kader <ashikulf@gmail.com>
2023-02-07 01:30:39 -0500

    Feb 7

---
+++ Code_Matters/src/jan_23/PrimeNum.java
@@ -0,0 +1,28 @@
+package jan_23;
+
+import java.util.Scanner;
+
+public class PrimeNum {
+    // check if the number is prime or not
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a range: ");
+        int range = sc.nextInt();
+        sc.close();
+
+        int count = 0;
+
+        for (int i = 1; i<= range; i++){
+            if (range % i == 0 ){
+                count++;
+            }
+        }
+        if (count == 2){
+            System.out.println("Prime");
+        }else {
+            System.out.println("Not Prime");
+        }
+
+
+    }
+}
