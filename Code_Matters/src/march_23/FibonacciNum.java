6e7e8426ca37f968bf86854ebbba6862637a2f08
Fahad_Kader <ashikulf@gmail.com>
2023-03-05 00:27:51 -0500

    march5

---
+++ Code_Matters/src/march_23/FibonacciNum.java
@@ -0,0 +1,33 @@
+package march_23;
+
+import java.util.Scanner;
+
+public class FibonacciNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Range: ");
+        int range = sc.nextInt();
+        sc.close();
+
+        FibonacciNum fibonacciNum = new FibonacciNum();
+        fibonacciNum.fibo(range);
+
+    }
+
+    void fibo(int range){
+        int a = 0;
+        int b = 1;
+        int temp = 0;
+        System.out.print(a + " " + b+" ");
+
+        while (range >= temp){
+            temp = a+b;
+            if (temp > range){
+                break;
+            }
+            System.out.print(temp+" ");
+            a = b;
+            b = temp;
+        }
+    }
+}
